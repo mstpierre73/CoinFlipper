afe77eebe108c5b147216f4aa76d6cc4c6f6e897
Marise St-Pierre <naranjaqc@hotmail.com>
2020-03-23 17:28:02 -0400

    add flip logic

--- src/CoinComponent/CoinComponent.jsx
+++ src/CoinComponent/CoinComponent.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './CoinComponent.css';
 
-function CoinComponent () {
+function CoinComponent (props) {
+    let img;
+    console.log('coinImg', props.coinValue)
+
+    if (props.coinValue) {
+        img = <img className='coinImg' src='head2.jpg' alt='coin head or coin tail'/>
+    } else {
+        img = <img className='coinImg' src='tail2.jpg' alt='coin head or coin tail'/>
+    }
+
     return (
       <div className="CoinComponent">
-  
+          {img}
       </div>
     );
   }
--- src/FlipperContainer/FlipperContainer.jsx
+++ src/FlipperContainer/FlipperContainer.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
-import CoinComponent from '../CoinComponent/CoinComponent;
+import CoinComponent from '../CoinComponent/CoinComponent';
 
-function FlipperContainer () {
-    return (
-      <div className="FlipperContainer">
-        <CoinComponent />
-      </div>
-    );
-  }
+class FlipperContainer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            faceToDisplay : true,
+            count: 0,
+            headCount : 0,
+            tailCount : 0
+        }
+    }
+
+    handleClick = () => {
+        console.log('onClick', this.state.faceToDisplay)
+        this.setCounter();
+    }
+
+    setCounter = () => {
+        this.setState({count: this.state.count + 1});
+        this.setState({faceToDisplay : !this.state.faceToDisplay});
+        if (this.state.faceToDisplay) {
+            this.setState({headCount: this.state.headCount +1});
+        } else {
+            this.setState({tailCount: this.state.tailCount +1});
+        }
+        console.log('setCounter', this.state.faceToDisplay);
+    }
+
+    render(){
+        return (
+            <div className="FlipperContainer">
+              <h1>Let's flip a coin</h1>
+              <CoinComponent coinValue={this.state.faceToDisplay}/>
+              <button onClick={this.handleClick}>Flip me !</button>
+              <p>Out of {this.state.count} flips, there have been {this.state.headCount} heads and {this.state.tailCount} tails.</p>
+            </div>
+        );
+    }
+}
 
 export default FlipperContainer;

2d6def5f61bcdeb9d81128c9cc5479ec79868247
Marise St-Pierre <naranjaqc@hotmail.com>
2020-03-23 15:56:59 -0400

    first commit

---
+++ src/CoinComponent/CoinComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CoinComponent () {
+    return (
+      <div className="CoinComponent">
+  
+      </div>
+    );
+  }
+
+export default CoinComponent;
---
+++ src/FlipperContainer/FlipperContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CoinComponent from '../CoinComponent/CoinComponent;
+
+function FlipperContainer () {
+    return (
+      <div className="FlipperContainer">
+        <CoinComponent />
+      </div>
+    );
+  }
+
+export default FlipperContainer;
